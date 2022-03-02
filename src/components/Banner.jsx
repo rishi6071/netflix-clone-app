29d73a26f1d7274f15eac1cba669e2a38ddea71e
Rishi Soni <rishisoni6071@gmail.com>
2022-03-02 21:14:00 +0530

    Added Overlay

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -22,7 +22,7 @@ const Banner = ({ fetchURI }) => {
         });
     };
     fetchData();
-  }, []);
+  }, [fetchURI]);
 
   function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
